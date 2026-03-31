33889b8524cdc6ea38ce92fcb26e1547428054be
ehoiguinho <igorlnss2003@gmail.com>
2026-03-30 22:30:32 -0300

    feat: adicionado autenticacao nas rotas

--- routes/locacaoRoute.js
+++ routes/locacaoRoute.js
@@ -1,17 +1,19 @@
 import express from 'express';
 import LocacaoController from '../controllers/locacaoController.js';
+import AuthMiddleware from '../middlewares/authMiddleware.js';
 const router = express.Router();
 
 let ctrl = new LocacaoController();
+let auth = new AuthMiddleware();
 
-router.post("/", (req, res) => {
+router.post("/", auth.validarToken.bind(auth), (req, res) => {
     //#swagger.tags = ['Locação']
     //#swagger.summary = "Inicia o processo de locação de imóvel"
 
     ctrl.locar(req, res);
 })
 
-router.get("/:id", (req, res) =>{
+router.get("/:id", auth.validarToken.bind(auth), (req, res) =>{
     //#swagger.tags = ['Locação']
     //#swagger.summary = "Faz uma busca do contrato de locação de um determinado usuário"
 

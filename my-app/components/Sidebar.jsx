f4fc83a1c3cff336ba1f04a9d54fac264637d584
igor <igorlnss2003@gmail.com>
2026-05-30 09:19:03 -0300

    ajustes sidebar

--- my-app/components/Sidebar.jsx
+++ my-app/components/Sidebar.jsx
@@ -1,7 +1,8 @@
 "use client"
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+
 
 
 

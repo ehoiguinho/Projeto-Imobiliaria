7d51b107c32d1c888332d9bcbe4bcedddf5bb83f
igor <150636747+ehoiguinho@users.noreply.github.com>
2026-09-11 09:09:52 -0300

    configure api proxy for production

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -4,6 +4,21 @@ const nextConfig: NextConfig = {
     turbopack: {
         root: __dirname,
     },
+
+    async rewrites() {
+        return [
+            {
+                source: "/api/uploads/:path*",
+                destination:
+                    "https://projeto-imobiliaria-api.onrender.com/uploads/:path*",
+            },
+            {
+                source: "/api/:path*",
+                destination:
+                    "https://projeto-imobiliaria-api.onrender.com/:path*",
+            },
+        ];
+    },
 };
 
 export default nextConfig;
